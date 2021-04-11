28f6fbbdd3979207f924109e53709df1c462bdb7
Jonathan Huata Vasquez <jonathan_huata@outlook.com>
2021-04-11 10:24:44 -0300

    se agrego favicon y el title

--- frontend/src/components/MiCarousel.js
+++ frontend/src/components/MiCarousel.js
@@ -13,15 +13,15 @@ import MiSlide from './MiSlide.js'
 
 const MiCarousel = ({gruposCiudades}) => {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
+  const [animating, setAnimating] = useState(false); //esto es para evitar usar los controles manuales mientras esta la animacion de cambio de slide
 
-  const next = () => {
+  const next = () => {  //actualiza el indexActual (el estado) al siguiente o a 0 si esta en el ultimo slide
     if (animating) return;
     const nextIndex = activeIndex === gruposCiudades.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   }
 
-  const previous = () => {
+  const previous = () => { //actualiza el indexActual (el estado) al anterior o al ultimo de los slide si esta en el primer slide
     if (animating) return;
     const nextIndex = activeIndex === 0 ? gruposCiudades.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
@@ -31,7 +31,8 @@ const MiCarousel = ({gruposCiudades}) => {
     if (animating) return;
     setActiveIndex(newIndex);
   }
-  //un array de CarouselItem que se inyectran en <Caroucel>
+
+  //un array de CarouselItem que se inyectan en <Caroucel>
   const slides = gruposCiudades.map((unGrupoCiudades,indice) => {
     return (
       <CarouselItem
@@ -52,6 +53,7 @@ const MiCarousel = ({gruposCiudades}) => {
         activeIndex={activeIndex}
         next={next}
         previous={previous}
+        interval = {2000}
       >
         <CarouselIndicators items={arrayNuevo} activeIndex={activeIndex} onClickHandler={goToIndex} />
         {slides}
